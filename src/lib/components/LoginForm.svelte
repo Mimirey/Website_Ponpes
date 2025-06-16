bbbd318136cc19408538af35cee8e46fe3bc8289
Mimirey <myraisadora7@gmail.com>
2025-06-16 15:25:38 +0700

    feat: [MI} login-api

--- src/lib/components/LoginForm.svelte
+++ src/lib/components/LoginForm.svelte
@@ -1,34 +1,39 @@
 <script>
 	import { goto } from "$app/navigation";
     import toast from 'svelte-french-toast';
-   
 
+    export let form;
+    export let enhance;
     let username='';
     let password='';
     let error = '';
-    
+     
 
-    const handleLogin=()=>{
-        if (username === 'ortu' && password === 'ortu123') {
-            goto('/dashboard');
+    // const handleLogin=()=>{
+    //     if (username === 'ortu' && password === 'ortu123') {
+    //         goto('/dashboard');
             
-        } else {
-            error='Username dan password harus valid!'
-            toast.error(error)
+    //     } else {
+    //         error='Username dan password harus valid!'
+    //         toast.error(error)
             
-        }
-    }
+    //     }
+    // }
 </script>
 
 
-<form on:submit|preventDefault={handleLogin} >
+<form method="POST" use:enhance >
 <label for="usn">
-    <input type="text" bind:value={username} placeholder="Username">
+     <input type="text" name="username" bind:value={username} placeholder="username" required  />
 </label>
 
 <label for="pass">
-    <input type="password" bind:value={password} placeholder="Password">
+    <input type="password" name="password" bind:value={password} placeholder="password" required />
 </label>
  <button type="submit">Login</button>
+
+ {#if form?.error}
+  <p class="text-red-500 mt-2">{form.error}</p>
+{/if}
 
 </form>
