3cb9bcb9d0418d4e749f5e211453b413792c1721
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2025-06-20 21:12:06 +0700

    fix: change url api to env file

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,64 +1,68 @@
 export const actions = {
-  default: async ({ request, cookies }) => {
-    try {
-      const formData = await request.formData();
-      const username = formData.get('username');
-      const password = formData.get('password');
+	default: async ({ request, cookies }) => {
+		try {
+			const formData = await request.formData();
+			const username = formData.get('username');
+			const password = formData.get('password');
 
-      const res = await fetch('https://admin.al-achsaniyyah.id/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-          U_NAME: username,
-          U_PASSWORD: password
-        })
-      });
+			const res = await fetch(import.meta.env.VITE_API_SERVER_URL + '/auth/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json'
+				},
+				body: JSON.stringify({
+					U_NAME: username,
+					U_PASSWORD: password
+				})
+			});
 
-      if (!res.ok) {
-        const errorText = await res.text();
-        console.error('API Error:', errorText);
-        return { error: 'Terjadi kesalahan saat login. Silakan coba lagi.' };
-      }
+			if (!res.ok) {
+				const errorText = await res.text();
+				console.error('API Error:', errorText);
+				return { error: 'Terjadi kesalahan saat login. Silakan coba lagi.' };
+			}
 
-      const data = await res.json();
+			const data = await res.json();
 
-      if (data.STATUS === 'SUCCESS') {
-        const user = data.PAYLOAD.USER;
+			if (data.STATUS === 'SUCCESS') {
+				const user = data.PAYLOAD.USER;
 
-        cookies.set('token', user.U_LOGIN_TOKEN, {
-          path: '/',
-          httpOnly: true,
-          sameSite: 'lax',
-          secure: false,
-          maxAge: 60 * 60 * 24
-        });
+				cookies.set('token', user.U_LOGIN_TOKEN, {
+					path: '/',
+					httpOnly: true,
+					sameSite: 'lax',
+					secure: false,
+					maxAge: 60 * 60 * 24
+				});
 
-        cookies.set('user', JSON.stringify({
-          id: user.U_ID,
-          name: user.NAME,
-          role: user.ROLE
-        }), {
-          path: '/',
-          httpOnly: false
-        });
+				cookies.set(
+					'user',
+					JSON.stringify({
+						id: user.U_ID,
+						name: user.NAME,
+						role: user.ROLE
+					}),
+					{
+						path: '/',
+						httpOnly: false
+					}
+				);
 
-        return {
-          success: true,
-          redirect: '/dashboard'
-        };
-      } else {
-        return {
-          error: data.MESSAGE ?? 'Login Gagal'
-        };
-      }
-    } catch (error) {
-      console.error('Fetch error:', error);
-      return {
-        error: 'Gagal terhubung ke server. Coba lagi nanti ya.'
-      };
-    }
-  }
+				return {
+					success: true,
+					redirect: '/dashboard'
+				};
+			} else {
+				return {
+					error: data.MESSAGE ?? 'Login Gagal'
+				};
+			}
+		} catch (error) {
+			console.error('Fetch error:', error);
+			return {
+				error: 'Gagal terhubung ke server. Coba lagi nanti ya.'
+			};
+		}
+	}
 };
